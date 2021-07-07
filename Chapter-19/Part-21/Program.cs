76d0281695249fab826338040009721da75e93ea
Vladimir1965 <zakharenkov1965@yandex.ru>
2021-07-07 11:52:10 +0300

    Добавляем English

--- Chapter-19/Part-21/Program.cs
+++ Chapter-19/Part-21/Program.cs
@@ -1,10 +1,18 @@
-﻿// Методы расширения, связанные с запросами, можно также использовать в самом
-// запросе, основываясь на синтаксисе запросов, предусмотренном в С#. И в действительности
-// это делается очень часто. Например, метод Average() используется в приведенной
-// ниже программе для получения последовательности, состоящей только из тех
-// значений, которые оказываются меньше среднего всех значений в массиве.
+﻿#region Russian
 
-//Использовать метод Average() вместе с синтаксисом запросов.
+/*
+
+ Методы расширения, связанные с запросами, можно также использовать в самом
+ запросе, основываясь на синтаксисе запросов, предусмотренном в С#. И в действительности
+ это делается очень часто. Например, метод Average() используется в приведенной
+ ниже программе для получения последовательности, состоящей только из тех
+ значений, которые оказываются меньше среднего всех значений в массиве.
+
+*/
+
+
+// Использовать метод Average() вместе с синтаксисом запросов.
+
 using System;
 using System.Linq;
 
@@ -29,27 +37,91 @@ class ExtMethods2
             Console.Write(i + " ");
         }
 
-        Console.WriteLine();
-
-        //Задержка программы.
         Console.ReadKey();
     }
 }
 
-// При выполнении этой программы получается следующий результат.
+/*
 
-// Среднее значение равно 5.6
-// Значения меньше среднего: 1 2 4 3
+ При выполнении этой программы получается следующий результат.
 
-// Обратите особое внимание в этой программе на следующий код запроса.
+ Среднее значение равно 5.6
+ Значения меньше среднего: 1 2 4 3
 
-// var ltAvg = from n in nums
-// let x = nums.Average()
-// where n<x
-// select n;
+ Обратите особое внимание в этой программе на следующий код запроса.
 
-// Как видите, переменной x в операторе let присваивается среднее всех значений
-// в массиве nums. Это значение получается в результате вызова метода Average() для
-// массива nums.
+ var ltAvg = from n in nums
+ let x = nums.Average()
+ where n<x
+ select n;
+
+ Как видите, переменной x в операторе let присваивается среднее всех значений
+ в массиве nums. Это значение получается в результате вызова метода Average() для
+ массива nums.
+
+*/
+
+#endregion
+
+#region English
+
+/*
+
+You can also use the query-related extension methods within a query based on the C#
+query syntax. In fact, it is quite common to do so. For example, this program uses Average( )
+to obtain a sequence that contains only those values that are less than the average of the
+values in an array.
+
+*/
+
+// Use Average() with the query syntax. 
+
+//using System;  
+//using System.Linq;  
+
+//class ExtMethods2
+//{
+//    static void Main()
+//    {
+
+//        int[] nums = { 1, 2, 4, 8, 6, 9, 10, 3, 6, 7 };
+
+//        var ltAvg = from n in nums
+//                    let x = nums.Average()
+//                    where n < x
+//                    select n;
+
+//        Console.WriteLine("The average is " + nums.Average());
+
+//        Console.Write("These values are less than the average: ");
+
+//        // Execute the query and display the results. 
+//        foreach (int i in ltAvg) Console.Write(i + " ");
+
+//        Console.ReadKey();
+//    }
+//}
+
+
+/*
+
+The output is shown here:
+
+The average is 5.6
+These values are less than the average: 1 2 4 3
+
+Pay special attention to the query:
+
+var ltAvg = from n in nums
+let x = nums.Average()
+where n < x
+select n;
+
+Notice in the let statement, x is set equal to the average of the values in nums. This value is
+obtained by calling Average() on nums.
+
+*/
+
+#endregion
 
 
